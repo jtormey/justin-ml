280e2a39d35da47a6583863482944e5c8e5ed972
Justin Tormey <justin@blockchain.com>
2016-11-10 15:21:40 -0500

    implement html generator

--- src/generator.js
+++ src/generator.js
@@ -1,4 +1,30 @@
 
+let renderAttrs = (attrs) => {
+  return attrs
+    .map(a => ` ${a.name}="${a.value}"`)
+    .join('')
+}
+
+let renderElem = (elem) => {
+  switch (elem.type) {
+    case 'TextNode':
+      return elem.value
+    case 'Element': {
+      let tag = elem.name
+      let attrs = renderAttrs(elem.attrs)
+      return [
+        '<' + tag + attrs + '>',
+        ...elem.children.map(renderElem),
+        '</' + tag + '>'
+      ].join('')
+    }
+    default:
+      throw new Error('received invalid type')
+  }
+}
+
 module.exports = (input) => {
-  return input
+  return input.children
+    .map(renderElem)
+    .join('')
 }
