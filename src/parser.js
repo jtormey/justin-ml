047e4991df28ca1544ae815d4f3c69f078a15ad3
Justin Tormey <justin@blockchain.com>
2016-11-10 20:54:12 -0500

    clean parser code

--- src/parser.js
+++ src/parser.js
@@ -2,28 +2,20 @@
 let vars = {}
 
 let setVar = (name, value) => {
-  if (vars[name] != null) {
-    throw new Error('variable already defined: ' + name)
-  } else {
-    vars[name] = value
-  }
+  if (vars[name] != null) throw new Error('variable already defined: ' + name)
+  else vars[name] = value
 }
 
 let getVar = (name) => {
   let val = vars[name]
-  if (val == null) {
-    throw new Error('variable not found: ' + name)
-  }
+  if (val == null) throw new Error('variable not found: ' + name)
   return String(val)
 }
 
 let get = (type, input) => {
   let token = input.shift()
-  if (token.type !== type) {
-    throw new Error('expected: ' + type)
-  } else {
-    return token
-  }
+  if (token.type !== type) throw new Error('expected: ' + type)
+  else return token.value
 }
 
 let takeUntil = (type, input) => {
@@ -48,42 +40,51 @@ let parseString = (input) => {
 let getAttrs = (input, attrs = []) => {
   let token = input.shift()
   let attr = { type: 'Attribute' }
+
   switch (token.type) {
-    case 'space':
-      return getAttrs(input, attrs)
-    case 'word':
-      attr.name = token.value
-      get('colon', input)
-      attr.value = input[0].type === 'variable'
-        ? getVar(input.shift().value)
-        : parseString(input)
-      attrs.push(attr)
-      return getAttrs(input, attrs)
     case 'open_bracket':
       return attrs
+    case 'space':
+      break
+
+    case 'word': {
+      attr.name = token.value
+      get('colon', input)
+      let isNextVar = input[0].type === 'variable'
+      attr.value = isNextVar ? getVar(get('variable', input)) : parseString(input)
+      attrs.push(attr)
+      break
+    }
+
     default:
       throw new Error(`expected type: space, word (received: ${token.type})`)
   }
+  return getAttrs(input, attrs)
 }
 
 let getChildren = (input, children = []) => {
-  if (input.length === 0) {
-    return children
-  }
   let token = input.shift()
+
   switch (token.type) {
+    case 'close_bracket':
+      return children
+    case 'space':
+      break
+    case 'newline':
+      break
+    case 'comment':
+      takeUntil('newline', input)
+      break
+
     case 'keyword': {
       get('space', input)
       let variable = get('variable', input)
       get('space', input)
       let value = parseString(input)
-      setVar(variable.value, value)
-      return getChildren(input, children)
+      setVar(variable, value)
+      break
     }
-    case 'close_bracket':
-      return children
-    case 'space':
-      return getChildren(input, children)
+
     case 'word': {
       children.push({
         type: 'Element',
@@ -91,44 +92,36 @@ let getChildren = (input, children = []) => {
         attrs: getAttrs(input),
         children: getChildren(input)
       })
-      if (input.length) {
-        getChildren(input, children)
-      }
       break
     }
+
     case 'quote': {
       input.unshift(token)
       children.push({
         type: 'TextNode',
         value: parseString(input)
       })
-      getChildren(input, children)
       break
     }
+
     case 'variable': {
       children.push({
         type: 'TextNode',
         value: getVar(token.value)
       })
-      getChildren(input, children)
       break
     }
-    case 'comment':
-      takeUntil('newline', input)
-      getChildren(input, children)
-      break
-    case 'newline':
-      getChildren(input, children)
-      break
+
     default:
       throw new Error(`expected type: word (received ${token.type})`)
   }
-  return children
+
+  return input.length > 0 ? getChildren(input, children) : children
 }
 
 module.exports = (input) => {
   return {
-    type: 'Document',
+    type: 'Root',
     children: getChildren(input)
   }
 }
